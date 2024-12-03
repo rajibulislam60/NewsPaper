75845fc7bd85345390f7070558fbb9fc1f71b111
rajibulislam60 <rajibulislam60707877@gmail.com>
2024-12-03 19:53:07 +0600

    start a new project as a newpaper

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='container my-4'>
+        <h1 className='text-[34px] font-bold leading-[38px] text-center'>Head line the news</h1>
+    </div>
+  )
+}
+
+export default Header
