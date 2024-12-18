c2e92a1d9ba6416fbe0ed473b3ba521bb0f12de0
rajibulislam60 <rajibulislam60707877@gmail.com>
2024-12-18 12:39:26 +0600

    env working

--- src/App.jsx
+++ src/App.jsx
@@ -7,40 +7,11 @@ import Loading from "./components/Loading";
 import axios from "axios";
 
 const App = () => {
-  const URL = "https://jsonplaceholder.typicode.com/users";
-
-  axios.get(URL).then((res)=>{
-    console.log(res.data)
-  })
-
-  const user = {
-    name:"RI Rajib",
-    email:"rirajveer@gmail.com",
-    username:"rajib",
-  }
-  axios.post(URL, user).then((res)=>{
-    console.log(res.data)
-  })
+  
 
 
-  const news = [
-    {
-      title: "Sample News 1",
-      alt: "Image of Sample News 1",
-      url: "https://example.com/sample-news-1",
-      urlToImage: "https://via.placeholder.com/150",
-      publishedAt: "2024-12-05T10:00:00Z",
-      source: { name: "Example Source 1" },
-    },
-    {
-      title: "Sample News 2",
-      alt: "image2",
-      url: "https://example.com/sample-news",
-      urlToImage: "https://via.placeholder.com/150",
-      publishedAt: "2024-12-05T10:00:00Z",
-      source: { name: "Example Source" },
-    },
-  ];
+
+
   return (
     <div>
       <Header category="technology" />
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+const rootElement = document.getElementById('root');
+
+if (process.env.NODE_ENV !== "development") {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+} else {
+  createRoot(rootElement).render(<App />);
+}
+
+
+console.log(process.env.REACT_APP_TEST);
